bead0710bd535f8cfe9fdb1051abbf7c6a0fc22a
Emilee Althoff <emileealthoff@gmail.com>
2021-11-29 16:32:53 -0600

    Image hides when browser hits a certain size

--- 480FinalProject_LogInACK.php
+++ 480FinalProject_LogInACK.php
@@ -12,7 +12,6 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-
    
     <title>Gallery w/ Bootstrap</title>
 </head>
@@ -65,7 +64,7 @@ echo '</li>';
 <section class="vh-100">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-sm-6 text-black">
+      <div class="col text-black">
 
         <div class="px-5 ms-xl-4">
           <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
@@ -82,8 +81,8 @@ header("Location: 480FinalProject_LogIn.php");
         </div>
 
       </div>
-      <div class="col-sm-6 px-0 d-none d-sm-block">
-        <img src="Images/abstract.jpg" alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
+      <div id="imgdiv" class="col px-0 d-none d-lg-block">
+        <img id="logimg" src="Images/abstract.jpg" alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
       </div>
     </div>
   </div>
--- 480FinalProject_LogOut.php
+++ 480FinalProject_LogOut.php
@@ -12,7 +12,6 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-
    
     <title>Log Out</title>
 </head>
@@ -64,7 +63,7 @@ echo '</li>';
 <section class="vh-100">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-sm-6 text-black">
+      <div class="col text-black">
 
         <div class="px-5 ms-xl-4">
           <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
@@ -79,8 +78,8 @@ echo '<p>You have successfully logged out.</p>'
         </div>
 
       </div>
-      <div class="col-sm-6 px-0 d-none d-sm-block">
-        <img src="Images/abstract.jpg" alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
+      <div id="imgdiv" class="col px-0 d-none d-lg-block">
+        <img id="logimg" src="Images/abstract.jpg" alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
       </div>
     </div>
   </div>
